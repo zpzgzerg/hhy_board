a89b1413b77a5a656949297576caf110d0dd6f63
zpzgzerg <zpzgzerg@naver.com>
2020-06-12 18:02:34 +0900

    HHY 보드 첫번째 커밋!

---
+++ src/main/java/xyz/zpzgzerg/hhy_board/HhyBoardApplication.java
@@ -0,0 +1,13 @@
+package xyz.zpzgzerg.hhy_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhyBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HhyBoardApplication.class, args);
+  }
+
+}
